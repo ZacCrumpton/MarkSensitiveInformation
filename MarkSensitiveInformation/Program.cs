a3962f0c9c3b8e931655d77bed38f5e08204cfb3
zcrumpton <z.crumpton@gmail.com>
2020-10-15 21:18:19 -0500

    Add project files.

---
+++ MarkSensitiveInformation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MarkSensitiveInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fourDigitInput = Console.ReadLine();
+
+            Console.WriteLine(fourDigitInput);
+        }
+    }
+}
